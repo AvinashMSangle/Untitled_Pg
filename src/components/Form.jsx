668d1a6f39214b7247803f49715bb52ad7712f8c
AvinashMSangle <avinashMS.3119@proton.me>
2024-12-18 08:33:55 +0000

    updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,79 +1,83 @@
+import { useForm } from "react-hook-form";
 import { TbFlareFilled } from "react-icons/tb";
 import Intro from "@/components/Intro";
-import { useState } from "react";
+import utils from "@/lib/utils";
 
+const services = [
+  "Website Design",
+  "Content",
+  "UX Design",
+  "Strategy",
+  "User Research",
+  "Other",
+];
 
 function Form() {
-  const [formData, setFormData] = useState({
-    fullname: "",
-    email: "",
-    message: "",
-  
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const services = [
-    "Website Design",
-    "Content",
-    "UX Design",
-    "Strategy",
-    "User Research",
-    "Other",
-  ];
- 
-
-
-  const handleSubmit = (e) => {
-    console.log(selectedservices);
-    e.preventDefault();
+  const handleFormSubmit = (data) => {                                                                      
+    const formData = new FormData();
+    formData.append(utils.fullname, data.fullname);
+    formData.append(utils.email, data.email);
+    formData.append(utils.message, data.message);
+    formData.append(utils.services, data.services);
+    fetch(utils.submitUrl, {
+      method: "POST",
+      mode:"no-cors",
+      body: formData,
+    }).then(()=>{
+      console.log("successfull",utils.entriesUrl)
+    })
   };
-
-  const handleChange = (value, property) => {
-    setFormData({ ...formData, [property]: value });
-  };
-
-  const [selectedservices, setSelectedServices] = useState([]);
-0
-  const handleCheckbox = (value, checked) => {
-   setSelectedServices((prevState)=>{
-   return checked ? [...prevState, value] : prevState.filter(item => item !== value)
-   })
-   };
 
   return (
     <>
       <Intro />
-      <form className="flex flex-col gap-1" action={import.meta.env.VITE_SUBMIT_URL} >
+      <form
+        className="flex flex-col gap-1"
+        onSubmit={handleSubmit(handleFormSubmit)}
+      >
         {/* Input */}
         <input
           type="text"
-          name={import.meta.env.VITE_NAME_FIELD}
+          {...register("fullname", { required: "Please enter your name" })}
           id="fullname"
           placeholder="Your name"
           className="border-b border-stone-700 bg-zinc-50 p-2 placeholder-slate-700 md:bg-lime-400"
-          required
-          value={formData.fullname}
-          onChange={(e) => handleChange(e.target.value, "fullname")}
         />
+        {errors.fullname && (
+          <p className="text-red-500">{errors.fullname.message}</p>
+        )}
         <input
           type="email"
-          name={import.meta.env.VITE_EMAIL_FIELD}
+          {...register("email", {
+            required: "Please enter your email",
+            pattern: {
+              value: /[\w]*@*[a-z]*\.*[\w]{3,}(\.)*(com)*(@gmail\.com)/g,
+              message: "Please enter a valid email address",
+            },
+          })}
           id="email"
           placeholder="your@company.com"
           className="border-b border-stone-700 bg-zinc-50 p-2 placeholder-slate-700 md:bg-lime-400"
-          required
-          value={formData.email}
-          onChange={(e) => handleChange(e.target.value, "email")}
         />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+
         <input
           type="text"
-          name={import.meta.env.VITE_MESSAGE_FIELD}
+          {...register("message", { required: "Please enter a message" ,
+            pattern:{
+              value: /^[a-zA-Z0-9\s]{1,}$/, 
+              message: "Please enter a valid message"} })}
           id="message"
           placeholder="Tell us a bit about your project..."
           className="h-24 border-b border-stone-700 bg-zinc-50 p-2 placeholder-slate-700 md:bg-lime-400"
-          required
-          value={formData.message}
-          onChange={(e) => handleChange(e.target.value, "message")}
         />
+        {errors.message&&<p className="text-red-500">{errors.message.message}</p>}
 
         <p className="my-5 text-zinc-800">How can we help?</p>
 
@@ -87,21 +91,22 @@ function Form() {
               >
                 <input
                   type="checkbox"
-                  name={import.meta.env.VITE_SERVICES_FIELD}
+                  {...register("services",{required:"Atleast Select One Service"})}
+
+                
                   value={service}
                   className="size-6"
-                  onChange={(e) => handleCheckbox(service, e.target.checked)}
                 />
                 {service}
               </label>
             );
           })}
+
+          {errors.services&&<p className="text-red-500">{errors.services.message}</p>}
         </section>
         <button
           type="submit"
           className="flex justify-center gap-2 rounded-lg bg-stone-950 p-2 text-white"
-
-                 
         >
           Let's get started
           <TbFlareFilled size={20} className="text-lime-500" />
